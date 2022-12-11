33478d5db7d61861c95c6e0d6fe5a1681fd5eef0
Naro <leejane311210@gmail.com>
2022-12-11 13:05:03 +0800

    Update auto_post.js

--- auto_post.js
+++ auto_post.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios')
 const notify = require('./sendNotify')
 
@@ -11,20 +12,18 @@ const h ={"accept": "application/json, text/plain, */*",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "traceparent": "00-f8b5a12993b6ed888eca31ca1b5b9e3f-3a8b6a99b6ecddc0-01",
-    "cookie": process.env.NorthSideCookie,
-    "Referer": "https://docs.qq.com/desktop/?u=99d3c99d84b040eaa062b5a5083a886e",
+    "cookie": process.env.TencentDocCookie,
+    "Referer": "https://docs.qq.com/desktop",
     "Referrer-Policy": "strict-origin-when-cross-origin"
 }
-const p = "folder_id=%2F&limit=40&list_type=6&order_type=0&sort_type=2&start=0&xsrf=9898ed4cafd43a31"
-
-//获取你的所有表单
-axios.post("https://docs.qq.com/cgi-bin/online_docs/doclist?u=99d3c99d84b040eaa062b5a5083a886e",p , {headers:h})
+//TODO 动态获取token
+const p = "folder_id=%2F&limit=40&list_type=6&order_type=0&sort_type=2&start=0&xsrf="+process.env.TencentDocCookie.match(/TOK=(\S*);/)[1]+""
+axios.post("https://docs.qq.com/cgi-bin/online_docs/doclist",p , {headers:h})
 .then((res)=>{
-    console.log(res.data.list_info)
+    console.log(res.data)
     arr = res.data.list_info
     for(var i = 0; i < arr.length; i++){
-      //匹配你设置的文档地址，并获取具体的文档padid
-        if(arr[i].doc_url == process.env.NorthSideUrl.replace(/^https:+/, "")){
+        if(arr[i].doc_url == process.env.DaoZhenUrl.replace(/^https:+/, "")){
             var d_id = arr[i].domain_id
             var p_id = arr[i].pad_id
             var padId = d_id+'$'+p_id
@@ -36,7 +35,6 @@ axios.post("https://docs.qq.com/cgi-bin/online_docs/doclist?u=99d3c99d84b040eaa0
 
 })
 
-//提交表单
 function post(padId){
     const header =  {
         "accept": "*/*",
@@ -48,16 +46,16 @@ function post(padId){
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
-        "cookie": process.env.NorthSideCookie,
-        "Referer": process.env.NorthSideUrl,
+        "cookie": process.env.TencentDocCookie,
+        "Referer": process.env.DaoZhenUrl,
         "Referrer-Policy": "strict-origin-when-cross-origin"
         }
-    const payload = "------WebKitFormBoundaryqSDreB2wBZrEFB8J\r\nContent-Disposition: form-data; name=\"data\"\r\n\r\n{\"global_padid\":\""+padId+"\",\"data\":\"[{\\\"id\\\":\\\"blank-1\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NorthSideP1+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1648443444768l1a8nyg1\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NorthSideP2+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1648443452119l1a8o447\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NorthSideP3+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1648443459227l1a8o9ln\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NorthSideP4+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false}]\",\"role\":{\"hideIds\":[]}}\r\n------WebKitFormBoundaryqSDreB2wBZrEFB8J--\r\n"
+    const payload = "------WebKitFormBoundaryqSDreB2wBZrEFB8J\r\nContent-Disposition: form-data; name=\"data\"\r\n\r\n{\"global_padid\":\""+padId+"\",\"data\":\"[{\\\"id\\\":\\\"SIMPLE1662714821565l7u9h399\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NAME+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1669210457760latot481\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.CLASS+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1669210476407latotim0\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.NUMBER+"\\\",\\\"dataType\\\":1,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1669210495369latotx8q\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.IDCard+"\\\",\\\"dataType\\\":6,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"SIMPLE1669210502669latou2vi\\\",\\\"type\\\":\\\"SIMPLE\\\",\\\"content\\\":\\\""+process.env.TEL+"\\\",\\\"dataType\\\":7,\\\"securityStatus\\\":false},{\\\"id\\\":\\\"CHECKBOX1669210517909latouemv\\\",\\\"type\\\":\\\"CHECKBOX\\\",\\\"content\\\":[\\\""+process.env.DaoZhenCheck+"\\\"],\\\"dataType\\\":1,\\\"selectedOptionIds\\\":[\\\"OPTIONlatouoqw\\\"]},{\\\"id\\\":\\\"RADIO1669210573239latovlbv\\\",\\\"type\\\":\\\"RADIO\\\",\\\"content\\\":[\\\""+process.env.TIME+"\\\"],\\\"dataType\\\":1,\\\"selectedOptionIds\\\":[\\\"OPTIONlatovnit\\\"]}]\",\"role\":{\"hideIds\":[]}}\r\n------WebKitFormBoundaryqSDreB2wBZrEFB8J--\r\n"
 
     axios.post('https://docs.qq.com/form/collect/submit',payload,{headers:header} )
     .then((res) => {
     r = res.statusText+JSON.stringify(res.data)
-    notify.sendNotify('北区核酸协助活动报名结果',r)
+    notify.sendNotify('导诊活动报名结果',r)
     console.log(`statusCode: ${r}`)
     console.log(res)
     })
